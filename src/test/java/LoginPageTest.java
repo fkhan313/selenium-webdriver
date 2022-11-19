bd81fc8f7e8a9771917a1a611dc28c7ef79fdb3d
fkhan313 <farhan.khan615@gmail.com>
2022-11-19 01:48:31 -0500

    Update files

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -18,18 +19,21 @@ public class LoginPageTest {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
-    @Test
+    @Test (description = "Testing login functionality")
     public void loginTest () {
+        Reporter.log("Application Started",true);
         driver.navigate().to("https://www.saucedemo.com/v1/");
         driver.manage().window().maximize();
         LoginPage loginPage= new LoginPage(driver);
         assertTrue(loginPage.isLoaded());
         loginPage.login();
+        Reporter.log("Clicked on Login button", true);
     }
 
     @AfterMethod
     public void tearDown () throws InterruptedException {
         Thread.sleep(3000);
         driver.quit();
+        Reporter.log("Browser closed",true);
     }
 }
