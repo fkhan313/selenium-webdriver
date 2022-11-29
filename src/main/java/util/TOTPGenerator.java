cbd3a0fe90519de4ee37c40051d999f982fc3e59
fkhan313 <farhan.khan615@gmail.com>
2022-11-28 23:06:39 -0500

    update otp generator class

--- src/main/java/util/TOTPGenerator.java
+++ src/main/java/util/TOTPGenerator.java
@@ -3,14 +3,16 @@ package util;
 import org.jboss.aerogear.security.otp.Totp;
 
 public class TOTPGenerator {
-    public static String getTwoFactorCode() {
+    public static void getTwoFactorCode() {
 
 
         // Sample secret key: see http://j.mp/2mOovSV
-        String secretKey = "6jm7n6xwitpjooh7ihewyyzeux7aqmw2";
+        //String secretKey = "6jm7n6xwitpjooh7ihewyyzeux7aqmw2";
+        String secretKey = "YQVLTX3KR5L4XKVKJ6RPHNY7NTSUEISJMRK5GTO6LYBFA3WHTFRNSFIMUHB5Y3J4ICNJ7DIM3R4YH5M4P5VKTCDH46RR7MPH7DKOU4I";
         Totp totp = new Totp(secretKey);
         String otp = totp.now();
-        return otp;
+        //return otp;
+        System.out.println(otp);
 
 
     }
